b81410a25cda358e9f1eacb9eaf29faecc148619
shkodm <dmytro.sashko@tum.de>
2019-05-07 12:30:49 +0200

    Add test build with two sample projects

--- setup_ccache_sync.sh
+++ setup_ccache_sync.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
-# starts rsync daemon for docker, that allows syncronizing ccache
-# between host and containers during image build stage
-mkdir -p $HOME/ccache
+# folder for this particular build
+PROJ_FOLDER=$1
+if [  "$#" -lt 1 ]; then
+  echo "Need to pass name of the folder where to store ccache for this build"
+  exit 1
+fi
 
+# create folder in case they are not cached yet
+mkdir -p $HOME/ccache
+mkdir -p "$HOME/ccache/${PROJ_FOLDER}"
+
+# starts rsync daemon for docker, that allows synchronization of ccache
+# between host and container during image build stage
 RSYNC_SERVER_IP=$( ip -4 -o addr show docker0 | awk '{print $4}' | cut -d "/" -f 1 )
 RSYNC_CONFIG=$(mktemp)
+# allow access from docker container (we create only one, so name is hardcoded)
 cat <<EOF > "${RSYNC_CONFIG}"
 [precice-docker-cache]
-        path = $HOME/ccache
+        path = $HOME/ccache/${PROJ_FOLDER}
         comment = preCICE docker image cache
         hosts allow = 172.17.0.2
         use chroot = false
